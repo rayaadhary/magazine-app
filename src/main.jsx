42e199d1d5d851b3e895369968252b4e3e292f02
rayaadhary <adharyr9@gmail.com>
2026-09-09 00:22:48 +0700

    gas

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { pdfjs } from 'react-pdf'
 
-pdfjs.GlobalWorkerOptions.workerSrc = '/pdf.worker.js'
+// react-pdf sets workerSrc to 'pdf.worker.mjs', override to serve from public/
+pdfjs.GlobalWorkerOptions.workerSrc = '/pdf.worker.mjs'
 
 import './index.css'
 import App from './App.jsx'
