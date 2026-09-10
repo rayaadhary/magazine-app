86ceacdbf925381f773763866f45db929657cd1d
rayaadhary <adharyr9@gmail.com>
2026-09-10 15:50:14 +0700

    feat: install tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),tailwindcss(),],
   server: {
     proxy: {
       '/api': 'http://localhost:8000',
