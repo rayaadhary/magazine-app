10b17b13a0692a3299c7d2a65ca2bdcde16a5279
rayaadhary <adharyr9@gmail.com>
2026-09-20 21:14:14 +0700

    fix ah

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
-import { StrictMode, useEffect } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { pdfjs } from 'react-pdf'
 import { Toaster } from 'sonner'
-import Lenis from 'lenis'
 
 pdfjs.GlobalWorkerOptions.workerSrc = '/pdf.worker.mjs'
 
@@ -16,29 +15,10 @@ const queryClient = new QueryClient({
   },
 })
 
-function SmoothScroll({ children }) {
-  useEffect(() => {
-    const lenis = new Lenis({
-      duration: 1.2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      touchMultiplier: 2,
-    });
-    function raf(time) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-    requestAnimationFrame(raf);
-    return () => lenis.destroy();
-  }, []);
-  return children;
-}
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <SmoothScroll>
-        <App />
-      </SmoothScroll>
+      <App />
       <Toaster position="bottom-right" richColors />
     </QueryClientProvider>
   </StrictMode>,
